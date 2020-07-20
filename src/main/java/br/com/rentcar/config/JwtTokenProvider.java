f7eb85d073d552ab01bbcd31d801e3e19061aa85
Iury Amaral <iury.amaral@hivecloud.com.br>
2020-07-20 00:10:07 -0300

    feature/rentcar-010-remover-carro-id: Funcionalidade de remocao de carro de usuario logado por id finalizada

--- src/main/java/br/com/rentcar/config/JwtTokenProvider.java
+++ src/main/java/br/com/rentcar/config/JwtTokenProvider.java
@@ -1,6 +1,5 @@
 package br.com.rentcar.config;
 
-import br.com.rentcar.model.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
@@ -11,7 +10,6 @@ import io.jsonwebtoken.UnsupportedJwtException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
