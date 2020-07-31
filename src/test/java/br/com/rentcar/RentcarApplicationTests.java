aeaf74518cc0814e263040c26ed6f30f7613f345
Iury Amaral <iury.amaral@hivecloud.com.br>
2020-07-31 02:40:38 -0300

    Remocao de testes

--- src/test/java/br/com/rentcar/RentcarApplicationTests.java
+++ src/test/java/br/com/rentcar/RentcarApplicationTests.java
@@ -1,8 +1,8 @@
-package br.com.rentcar;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RentcarApplicationTests {
-}
+//package br.com.rentcar;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RentcarApplicationTests {
+//}
